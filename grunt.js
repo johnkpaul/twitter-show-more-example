64b5d3eb706a4ed175a3bad72a2932535ae9f42b
John Paul <john@johnkpaul.com>
2012-08-18 15:34:17 -0400

    initial commit, add grunt server with twitter api support

---
+++ grunt.js
@@ -0,0 +1,85 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  var connect = require('connect');
+  var http = require('http');
+  var url = require('url');
+  var config = require('./config');
+
+  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('server', 'Start a connect server for twitter API', function() {
+    grunt.log.writeln('starting connect server on port 3000');
+
+
+    var twitter = new require('ntwitter')(config)
+    var done = this.async();
+    var app = connect()
+      .use(connect.logger('dev'))
+      .use(function(req, res){
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        var since_id = query['since_id'];
+        if(!since_id){ since_id = 236905249166741500; }
+        twitter.search('NYC OR #nyc OR new york city', {since_id:since_id}, function(err, data) {
+          res.end(JSON.stringify(data.results));
+        });
+      });
+     http.createServer(app).listen(3000).on('close', done);
+  });
+
+
+};
---
+++ src/show-more-view.js
@@ -0,0 +1,12 @@
+/*
+ * show-more-view
+ * https://github.com/jpaul/show-more-view
+ *
+ * Copyright (c) 2012 John K. Paul
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+
+}(jQuery));
